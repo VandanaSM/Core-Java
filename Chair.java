40ebe39c182946755d917cdccc5b5e1b196d1184
VandanaSM <Vandanasm.xworkz@gmail.com>
2022-06-21 15:48:37 +0530

    committing Variables examples

---
+++ Chair.java
@@ -0,0 +1,15 @@
+class Chair{
+
+public static void main(String[] args){
+
+ String name= "nilkamal";
+ String colour= "brown";
+ int price=1200;
+
+ System.out.println("The name of Chair is " +name );
+ System.out.println("Color of Chair is " +colour );
+ System.out.println("Price of Chair is " +price );
+
+}
+
+}
