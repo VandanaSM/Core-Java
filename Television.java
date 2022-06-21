a009d9dd47031add848a65033428395a14a0b2b6
VandanaSM <Vandanasm.xworkz@gmail.com>
2022-06-21 15:30:46 +0530

    committing Variables examples

--- Television.java
+++ Television.java
@@ -1,16 +1,19 @@
 class Television{
 
+    
+ static String name="Samsung";
+ static String color="black";
+ static int price=54990;
+ static String size="43inch";
+ static String connectorType="Wifi,Usb,Ethernet,Hdml";
 public static void main(String[] args){
 
- String name="Sony";
- String color="black";
- int price=42000;
- String size="43inch";
 
  System.out.println("Name of television is " +name);
  System.out.println("Color of television is " +color);
  System.out.println("Price of television is " +price);
  System.out.println("Size of television is " +size);
+ System.out.println("Connector type of television is " +connectorType);
 
 }
 
