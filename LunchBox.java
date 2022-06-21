bfa8318a6bb15d246b09c43fed08e683d70e5fc5
VandanaSM <Vandanasm.xworkz@gmail.com>
2022-06-21 15:42:14 +0530

    committing Variables examples

---
+++ LunchBox.java
@@ -0,0 +1,18 @@
+class LunchBox{
+
+    
+ static String name="Vaya tyffyn";
+ static int price=2690;
+ static int weight=600;
+ static int volume=600;
+public static void main(String[] args){
+
+
+ System.out.println("Name of LunchBox is " +name);
+ System.out.println("Volume of LunchBox is " +volume);
+ System.out.println("Price of LunchBox is " +price);
+ System.out.println("Weight LunchBox is " +weight);
+
+}
+
+}
