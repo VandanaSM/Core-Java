38190d339cafd9b35f642e3d073c947431363584
VandanaSM <Vandanasm.xworkz@gmail.com>
2022-06-21 10:43:32 +0530

    committing Variable files

---
+++ Bluetooth.java
@@ -0,0 +1,15 @@
+class Bluetooth{
+
+public static void main(String[] args){
+
+ String bluetoothName= "Boat";
+ String colour= "black";
+ int price=1400;
+
+ System.out.println("The name of bluetooth is " +bluetoothName );
+ System.out.println("Color of bluetooth is " +colour );
+ System.out.println("Price of bluetooth is " +price );
+
+}
+
+}
---
+++ Television.java
@@ -0,0 +1,17 @@
+class Television{
+
+public static void main(String[] args){
+
+ String name="Sony";
+ String color="black";
+ int price=42000;
+ String size="43inch";
+
+ System.out.println("Name of television is " +name);
+ System.out.println("Color of television is " +color);
+ System.out.println("Price of television is " +price);
+ System.out.println("Size of television is " +size);
+
+}
+
+}
