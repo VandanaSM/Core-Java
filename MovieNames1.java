6931fd903a9ca988450fff73cc3c86cc8d4ee228
VandanaSM <Vandanasm.xworkz@gmail.com>
2022-06-21 15:36:13 +0530

    committing Variables examples

---
+++ MovieNames1.java
@@ -0,0 +1,63 @@
+class MovieNames1{
+ 
+  static String movieName1 = "The Kashmir Files";
+  static String movieName2 = "URI The Surgical Strike";
+  static String movieName3 = "Mission Mangal";
+  static String movieName4 = "3 Idiots";
+  static String movieName5 = "Chhichore";
+  static String movieName6 = "M.S.Dhoni the untold story";
+  static String movieName7 = "Airlift";
+  static String movieName8 = "Rustom";
+  static String movieName9 = "Raazi";
+  static String movieName10 = "Ek Villain";
+  static String movieName11 = "Pink";
+  static String movieName12 = "Neerja";
+  static String movieName13= "Chak de india";
+  static String movieName14= "Ajab prem ki gazab kahani";
+  static String movieName15= "Taare zameen par";
+  static String movieName16= "Raanjhanaa";
+  static String movieName17= "Rang de Basanti";
+  static String movieName18= "Jaane tu...Ya jaane na";
+  static String movieName19= "Mohabbatein";
+  static String movieName20= "Thappad";
+  static String movieName21= "Tere naal love hogoya";
+  static String movieName22= "Sholay";
+  static String movieName23= "Saathiya";  
+  static String movieName24= "Phoonk";
+  static String movieName25= "Ishq Vishq";
+  
+  public static void main(String[] args){
+  
+  
+  System.out.println(movieName1);
+  System.out.println(movieName2); 
+  System.out.println(movieName3);
+  System.out.println(movieName4); 
+  System.out.println(movieName5);
+  System.out.println(movieName6); 
+  System.out.println(movieName7);
+  System.out.println(movieName8); 
+  System.out.println(movieName9);
+  System.out.println(movieName10); 
+  System.out.println(movieName11);
+  System.out.println(movieName12); 
+  System.out.println(movieName13);
+  System.out.println(movieName14); 
+  System.out.println(movieName15);
+  System.out.println(movieName16); 
+  System.out.println(movieName17);
+  System.out.println(movieName18); 
+  System.out.println(movieName19);
+  System.out.println(movieName20); 
+  System.out.println(movieName21);
+  System.out.println(movieName22); 
+  System.out.println(movieName23);
+  System.out.println(movieName24); 
+  System.out.println(movieName25);
+
+  }
+
+
+
+
+}
---
+++ MovieNames3.java
@@ -0,0 +1,63 @@
+class MovieNames3{
+ 
+  static String movieName1 = "The Pursiut of Happiness";
+  static String movieName2 = "Bangalore days";
+  static String movieName3 = "Mahanati";
+  static String movieName4 = "Kali";
+  static String movieName5 = "Charlie";
+  static String movieName6 = "Ustad Hotel";
+  static String movieName7 = "Kurup";
+  static String movieName8 = "Vaayai moodi pesavum";
+  static String movieName9 = "Solo";
+  static String movieName10 = "Lagaan";
+  static String movieName11 = "Life of Pi";
+  static String movieName12 = "Everest";
+  static String movieName13= "The Grey";
+  static String movieName14= "Crawl";
+  static String movieName15= "Gravity";
+  static String movieName16= "127 Hours";
+  static String movieName17= "The Impossible";
+  static String movieName18= "The Martain";
+  static String movieName19= "The revenant";
+  static String movieName20= "Cast Away";
+  static String movieName21= "Trapped";
+  static String movieName22= "Walkabout";
+  static String movieName23= "Buried";  
+  static String movieName24= "The Hijacking";
+  static String movieName25= "Into the wild";
+  
+  public static void main(String[] args){
+  
+  
+  System.out.println(movieName1);
+  System.out.println(movieName2); 
+  System.out.println(movieName3);
+  System.out.println(movieName4); 
+  System.out.println(movieName5);
+  System.out.println(movieName6); 
+  System.out.println(movieName7);
+  System.out.println(movieName8); 
+  System.out.println(movieName9);
+  System.out.println(movieName10); 
+  System.out.println(movieName11);
+  System.out.println(movieName12); 
+  System.out.println(movieName13);
+  System.out.println(movieName14); 
+  System.out.println(movieName15);
+  System.out.println(movieName16); 
+  System.out.println(movieName17);
+  System.out.println(movieName18); 
+  System.out.println(movieName19);
+  System.out.println(movieName20); 
+  System.out.println(movieName21);
+  System.out.println(movieName22); 
+  System.out.println(movieName23);
+  System.out.println(movieName24); 
+  System.out.println(movieName25);
+
+  }
+
+
+
+
+}
